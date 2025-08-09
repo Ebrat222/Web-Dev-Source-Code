c33f7939e81d1565c3d30e314f725201114e5e05
EBRAT HOSSEN <105649400+Ebrat222@users.noreply.github.com>
2025-08-09 14:06:25 +0600

    Create details.js

---
+++ Junk-Source/details.js
@@ -0,0 +1,31 @@
+Web-Dev-Source-Code {
+	HTML {
+		Structural Tags
+		Text Formatting Tags
+		Link & Navigation Tags
+		Media Tags
+		Sectioning Tags
+		List Tags
+		Table Tags
+		Form Tags
+		Scripting & Programmatic Tags
+		Metadata & Miscellaneous Tags
+		Core Semantic Tags
+		Media & Description Tags
+		Interactive Semantic Tags
+	}
+	CSS {
+		Box Model Properties
+		Color and Background
+		Text and Font Styling
+		Positioning and Layout
+		Responsive Design
+		Transitions and Animations
+		Visibility and Overflow
+		Advanced Selectors
+		
+	}
+	JavaScript {
+		
+	}
+}
